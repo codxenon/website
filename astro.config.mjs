7a0d54b2e5e2a67d98fc907079d01b1d365f6503
Michael Oliver <michael.oliver_@outlook.com>
2026-04-18 23:34:18 +0100

    refactor: add starlight docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,44 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import cloudflare from "@astrojs/cloudflare";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://codxenon.dev",
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  fonts: [
-    {
-      provider: fontProviders.fontsource(),
-      name: "Barlow Condensed",
-      cssVariable: "--font-barlow-condensed",
-      weights: [700, 900],
-    },
-    {
-      provider: fontProviders.fontsource(),
-      name: "Barlow",
-      cssVariable: "--font-barlow",
-      weights: [400, 500],
-    },
-    {
-      provider: fontProviders.fontsource(),
-      name: "Share Tech Mono",
-      cssVariable: "--font-share-tech-mono",
-      weights: [400],
-    },
-  ],
   adapter: cloudflare({
     imageService: "compile",
   }),
+  integrations: [
+    starlight({
+      title: "Cod Xenon",
+      favicon: "/favicon.ico",
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.com/invite/wCgjAMEQU5",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Start Here",
+          items: ["getting-started"],
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+      ],
+    }),
+  ],
 });
